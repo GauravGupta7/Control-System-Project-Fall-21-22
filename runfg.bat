4cad812349274452e0195bd844ea79cbd54b53af
GauravGupta7 <gaurav.gupta2019@vitstudent.ac.in>
2021-11-06 19:50:06 +0530

    Initial Commit

---
+++ runfg.bat
@@ -0,0 +1,5 @@
+D:
+cd D:\FlightGear 2020.3
+
+SET FG_ROOT=D:\FlightGear 2020.3\data
+.\\bin\fgfs --aircraft=dhc2F --fdm=null --native-fdm=socket,in,30,localhost,5502,udp --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --enable-freeze --airport=KSFO --runway=10L --altitude=7224 --heading=113 --offset-distance=4.72 --offset-azimuth=0 --prop:/sim/rendering/shaders/quality-level=0
